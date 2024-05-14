6628e5c0a5614d60202c9c9c9ffe3d2fd35da4c6
amitk-vmware <akumar56@vmware.com>
2024-05-14 19:23:25 +0530

    Add code for cases when delimiter is a special character other than (,) comma

--- code.rb
+++ code.rb
@@ -9,7 +9,10 @@ class StringOperation
       raise 'invalid argument' if arg[-1] == "\n"
     end
 
-    numbers = arg.split(/[\n,]/).map(&:strip) # separate numbers based on the delimiter and stip away any white spaces
+    # fetch delimiter when it is other than (,)
+    delimiter = arg[2] if arg.start_with?("//") 
+
+    numbers = arg.split(/[\n#{delimiter}]/).map(&:strip) # separate numbers based on the delimiter and stip away any white spaces
     numbers = numbers.map(&:to_i) # type conversion of all the numbers to integer from string
     numbers.sum # return their total sum
   end
