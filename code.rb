efeaeecdcae8e77df8f007a8a5055c8f5f4a8e17
amitk-vmware <akumar56@vmware.com>
2024-05-14 19:08:46 +0530

    Add code for the following cases
      + when string contains \n character
      + when newline character is present in the end

--- code.rb
+++ code.rb
@@ -2,10 +2,14 @@ class StringOperation
 
   # arg is a list of number separated by a delimiter
   def add(arg)
-    return 0 if arg == ""
+    return 0 if arg == ''
 
     delimiter = ','
-    numbers = arg.split(',').map(&:strip) # separate numbers based on the delimiter and stip away any white spaces
+    if arg.include?("\n")
+      raise 'invalid argument' if arg[-1] == "\n"
+    end
+
+    numbers = arg.split(/[\n,]/).map(&:strip) # separate numbers based on the delimiter and stip away any white spaces
     numbers = numbers.map(&:to_i) # type conversion of all the numbers to integer from string
     numbers.sum # return their total sum
   end
