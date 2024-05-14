9dc18010cb6938eba565f40fd209e58374539a72
amitk-vmware <akumar56@vmware.com>
2024-05-14 18:43:51 +0530

    Add code to handle the case when string of n numbers is separated by (,) delimiter

--- code.rb
+++ code.rb
@@ -4,6 +4,9 @@ class StringOperation
   def add(arg)
     return 0 if arg == ""
 
-    
+    delimiter = ','
+    numbers = arg.split(',').map(&:strip) # separate numbers based on the delimiter and stip away any white spaces
+    numbers = numbers.map(&:to_i) # type conversion of all the numbers to integer from string
+    numbers.sum # return their total sum
   end
 end
