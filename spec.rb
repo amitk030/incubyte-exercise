013efd47950d15bcdaf10831119a0d111ba765fb
amitk-vmware <akumar56@vmware.com>
2024-05-14 18:33:30 +0530

    Add spec for the case when arguments are present and separated by (,) delimiter

--- spec.rb
+++ spec.rb
@@ -11,4 +11,14 @@ RSpec.describe StringOperation do
       expect(calculator.add(arg)).to eq(0)
     end
   end
+
+  context 'when argument is present' do
+    context 'when delimiter is comma' do
+      let(:arg) {"1,2, 3, 4"}
+
+      it 'returns their sum' do
+        expect(calculator.add(arg)).to eq(10)
+      end
+    end
+  end
 end
